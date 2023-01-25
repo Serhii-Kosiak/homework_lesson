51f6eb9f9d5e2283087416f7b49e9c4d2ee0c592
Serhii-Kosiak <stylus.com.ua@gmail.com>
2023-01-25 23:50:09 +0200

    домашнее задание 3

---
+++ HW3/task2.py
@@ -0,0 +1,15 @@
+# You have the number 2 as a variable. Multiply it by 2 in two ways.
+# Accordingly, you need to divide it in 2 different ways by 2.
+
+number = 2
+
+print(number * 2)
+print(number ** 2)
+print(int(number / 0.5))
+
+print(int(number / 2))
+print(number // 2)
+
+number *= 2  # number = number * 2, 3й способ умножения, чтоб не использовать дробь и деление
+
+print(number)
