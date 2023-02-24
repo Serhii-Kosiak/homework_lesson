3e68725fc2de74dc7ce5fa1e528242fde2c2a670
Serhii-Kosiak <stylus.com.ua@gmail.com>
2023-02-25 00:38:49 +0200

    task4 remaster

--- HW8/task4.py
+++ HW8/task4.py
@@ -1,12 +1,5 @@
 
 
-def has_three(num: [int, float]):
-    return '3' in str(num)
-
-
 numbers = range(1, 1001)
-# numbers_with_three = filter(has_three, numbers)
-numbers_with_three = filter(lambda num: '3' in str(num), numbers)
-
-for num in numbers_with_three:
-    print(num)
+result = [number for number in numbers if '3' in str(number)]
+print(result)
